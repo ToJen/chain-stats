f7ecae36332347f6ef1016bb5f578701eb19de2b
Tomisin <tomisinjenrola@gmail.com>
2018-10-05 22:28:40 -0700

    Setup initial apollo react

---
+++ frontend/src/components/RootContainer.js
@@ -0,0 +1,98 @@
+import React, { Component, Fragment } from 'react'
+import {
+  NavLink,
+  Link,
+  BrowserRouter as Router,
+  Route,
+  Switch,
+} from 'react-router-dom'
+import FeedPage from './FeedPage'
+import DraftsPage from './DraftsPage'
+import CreatePage from './CreatePage'
+import DetailPage from './DetailPage'
+import PageNotFound from './PageNotFound'
+
+
+class RootContainer extends Component {
+
+
+
+  render() {
+    return (
+      <Router>
+        <Fragment>
+          {this.renderNavBar()}
+          {this.renderRoute()}
+        </Fragment>
+      </Router>
+    )
+  }
+
+  renderNavBar() {
+    return (
+      <nav className="pa3 pa4-ns">
+        <Link className="link dim black b f6 f5-ns dib mr3" to="/" title="Feed">
+          Blog
+        </Link>
+        <NavLink
+          className="link dim f6 f5-ns dib mr3 black"
+          activeClassName="gray"
+          exact={true}
+          to="/"
+          title="Feed"
+        >
+          Feed
+        </NavLink>
+        {this.props.data &&
+          this.props.data.me &&
+          this.props.data.me.email &&
+          this.state.token && (
+            <NavLink
+              className="link dim f6 f5-ns dib mr3 black"
+              activeClassName="gray"
+              exact={true}
+              to="/drafts"
+              title="Drafts"
+            >
+              Drafts
+            </NavLink>
+          )}
+
+        {this.props.data &&
+          this.props.data.me &&
+          this.props.data.me.email &&
+          this.state.token && (
+            <Link
+              to="/create"
+              className="f6 link dim br1 ba ph3 pv2 fr mb2 dib black"
+            >
+              + Create Draft
+            </Link>
+          )}
+      </nav>
+    )
+  }
+
+  renderRoute() {
+    return (
+      <div>
+        <Switch>
+          <Route exact path="/" component={FeedPage} />
+          <Route
+            path="/drafts"
+            component={DraftsPage}
+          />
+          <Route
+            path="/create"
+            component={CreatePage}
+          />
+          <Route path="/post/:id" component={DetailPage} />
+          <Route component={PageNotFound} />
+        </Switch>
+      </div>
+    )
+  }
+}
+
+
+export default RootContainer
---
+++ frontend/src/index.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { HttpLink, InMemoryCache, ApolloClient } from 'apollo-client-preset'
+import { WebSocketLink } from 'apollo-link-ws'
+import { ApolloLink, split } from 'apollo-link'
+import { getMainDefinition } from 'apollo-utilities'
+import RootContainer from './components/RootContainer'
+import { ApolloProvider } from 'react-apollo'
+
+
+
+const httpLink = new HttpLink({ uri: 'http://localhost:4000' })
+
+
+
+// authenticated httplink
+
+const wsLink = new WebSocketLink({
+  uri: `ws://localhost:4000`,
+  options: {
+    reconnect: true,
+  },
+})
+
+const link = split(
+  // split based on operation type
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query)
+    return kind === 'OperationDefinition' && operation === 'subscription'
+  },
+  wsLink,
+  httpLink,
+)
+
+// apollo client setup
+const client = new ApolloClient({
+  link: ApolloLink.from([link]),
+  cache: new InMemoryCache(),
+  connectToDevTools: true,
+})
+
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <RootContainer />
+  </ApolloProvider>,
+  document.getElementById('root'),
+)
