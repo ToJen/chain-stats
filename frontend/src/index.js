f31c81ce81b32d15b20aaa3f137b039eb784ab73
Tomisin <tomisinjenrola@gmail.com>
2018-10-06 02:08:03 -0700

    updated gql url

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -9,14 +9,14 @@ import { ApolloProvider } from 'react-apollo'
 
 
 
-const httpLink = new HttpLink({ uri: 'http://localhost:3001' })
+const httpLink = new HttpLink({ uri: 'http://localhost:3001/graphql' })
 
 
 
 // authenticated httplink
 
 const wsLink = new WebSocketLink({
-  uri: `ws://localhost:3001`,
+  uri: `ws://localhost:3001/graphql`,
   options: {
     reconnect: true,
   },
